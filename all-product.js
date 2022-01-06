95b692bc535a3825fed0ea155c6a265ff6271a0a
hieuntcasso <94815095+hieuntcasso@users.noreply.github.com>
2022-01-06 15:05:04 +0700

    add xml

--- all-product.js
+++ all-product.js
@@ -1,33 +1,26 @@
-function LoadData() {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
+// loadXMLDoc()
+function loadXMLDoc() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             myFunction(this);
         }
+    };
+    xmlhttp.open("GET", "product.xml", true);
+    xmlhttp.send();
+}
+function myFunction(xml) {
+    var i;
+    var xmlDoc = xml.responseXML;
+    console.log(xmlDoc);
+    let table 
+    var x = xmlDoc.getElementsByTagName("CD");
+    for (i = 0; i <x.length; i++) {
+      table += "<tr><td>" +
+      x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
+      "</td><td>" +
+      x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
+      "</td></tr>";
     }
-    xhr.open("GET", "product.xml", true);
-    xhr.send();
-
-    function myFunction(xml) {
-        var xmlDoc = xhr.responseXML;
-        console.log(xmlDoc);
-        var i;
-        var ul_list = "";
-        var x = xmlDoc.getElementsByTagName("product");
-        for (i = 0; i < x.length; i++) {
-            ul_list += "<li class=&quot;product-item&quot;>" +
-                "<div class=&quot;product-top&quot;>" +
-                +"<a class=&quot;product-thumb&quot;>" +
-                "<img src=&quot;image/all-product/" + [i + 1] + ".jpg&quot;/> </a>" +
-                "<a class=&quot;buy-now&quot;>Mua ngay</a> </div>" +
-                "<div class=&quot;product-info&quot;>" +
-                "<a class=&quot;product-category&quot;>" +
-                x[i].getElementsByTagName("type")[0].childNodes[0].nodeValue + "</a>" +
-                "<a  class=&quot;product-name&quot;>" +
-                x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue + "</a>" +
-                "<div  class=&quot;product-price&quot;>" +
-                x[i].getElementsByTagName("price")[0].childNodes[0].nodeValue + "</div> </div> </li>";
-        }
-        document.getElementById("products").innerHTML = ul_list;
-    }
+    document.getElementById("products").innerHTML = table;
 }
